a332c9b4b22dc4cc6e979441df426e54df4c3922
jimngmerkle <126862696+jimngmerkle@users.noreply.github.com>
2024-11-26 12:09:01 +0100

    auth update

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,21 +1,22 @@
 import React, { createContext, useState, useContext } from "react";
 
-// Create AuthContext
 const AuthContext = createContext();
 
 // Create a provider to wrap the app and provide global state
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // console.log("AuthProvider rendered. isLoggedIn:", isLoggedIn);
+  const [email, setEmail] = useState(''); 
 
   const logout = () => {
-    console.log('User logged out.')
+    console.log('User logged out.');
     setIsLoggedIn(false);
+    setEmail(''); 
     exponea.anonymize();
+    window.location.href = "/";
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, email, setEmail, logout }}>
       {children}
     </AuthContext.Provider>
   );
@@ -24,6 +25,5 @@ export const AuthProvider = ({ children }) => {
 // Custom hook for easy access to AuthContext
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  // console.log("useAuth called. Context:", context);
   return context;
 };
