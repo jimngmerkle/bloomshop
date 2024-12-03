56ed937c81a133d549f27299e5a45064d2d77b6e
jimngmerkle <126862696+jimngmerkle@users.noreply.github.com>
2024-12-03 11:45:58 +0100

    update methods

--- src/pages/methodspage/Methodspage.jsx
+++ src/pages/methodspage/Methodspage.jsx
@@ -1,71 +1,16 @@
-import React, { useState } from 'react';
-import { useAuth } from "../../utils/AuthContext"; 
-import toast from "react-hot-toast";
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Methodsform from "../../components/Methodsform/Methodsform";
 
-const Methodsform = () => {
-  const { email } = useAuth(); 
-  const [method, setMethod] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    try {
-      eval(method.trim());
-      console.log(method.trim());
-      toast.success('Method submitted!');
-    } catch (error) {
-      console.error('Error executing method:', error);
-      toast.error('Error executing method');
-    }
-  };
-
+const Methodspage = ({ cartItems }) => {
   return (
-    <div className="content">
-      <div className="two-column-layout">
-        <div className="left-column">
-          Current user: <strong>{email}</strong>
-          <br />
-          <p>Example methods: </p>
-          <br />
-          <pre>
-{`exponea.track('purchase', {
-purchase_status: 'success',
-product_list: [
-{ product_id: "abc123", quantity: 2 },
-{ product_id: "abc456", quantity: 1 }
-],
-total_price: 7.99,
-payment_type: 'credit_card'
-});
-
-exponea.update({
-email: 'gordon.freeman@blackmesa.com',
-first_name: 'Gordon',
-last_name: 'Freeman',
-company: 'Blackmesa'
-});`
-}
-        </pre>
-        </div>
-        <div className="right-column">
-          <form onSubmit={handleSubmit} id="form">
-            <label htmlFor="method">Enter SDK method:</label>
-            <br />
-            <textarea
-              id="method"
-              name="method"
-              value={method}
-              onChange={(e) => setMethod(e.target.value)}
-              className="event-textarea"
-              placeholder="Enter SDK method"
-              required
-            />
-            <button className="btn-primary" type="submit">Submit</button>
-          </form>
-        </div>
-      </div>
-      <br />
-    </div>
+    <>
+      <Header cartItems={cartItems} />
+      <Methodsform />
+      <Footer />
+    </>
   );
 };
 
-export default Methodsform;
+export default Methodspage;
